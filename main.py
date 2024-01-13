369e0e0d5622b48a4c007a91e1e7a211545d7fce
kolom <s_kolomeetzc@ukr.net>
2024-01-13 21:20:35 +0100

    homework 4 begin, fourth

--- main.py
+++ main.py
@@ -16,13 +16,28 @@
 
 ############## 2 ################
 
-num1 = list(map(int,input("Введіть ряд чисел через пробіл: ").split()))
-num2 = int(input("Введіть число для пошуку: "))
-
-num3 = 0
-for i in num1:
-   if i==num2:
-       num3+=1
-print(f"Число {num2} зустрічається {num3} разів")
+# num1 = list(map(int,input("Введіть ряд чисел через пробіл: ").split()))
+# num2 = int(input("Введіть число для пошуку: "))
+#
+# num3 = 0
+# for i in num1:
+#    if i==num2:
+#        num3+=1
+# print(f"Число {num2} зустрічається {num3} разів")
 
 ################# 3 ###################
+
+string = input("Введіть рядок: ")
+search_word = input("Введіть слово для пошуку: ")
+replace_word = input("Введіть слово, щоб замінити його на: ")
+words = string.split()
+modified_words = []
+for word in words:
+   if word == search_word:
+       modified_words.append(replace_word)
+   else:
+       modified_words.append(word)
+modified_string = " ".join(modified_words)
+print(modified_string)
+
+################### 4 #################
